335385ee5c6b367f3912d7b361a5dfc12539f526
fresh <1326191992@qq.com>
2021-07-10 21:06:50 +0800

    1

--- project-reactor/src/main/java/com/project/reactor/time/TimeEat.java
+++ project-reactor/src/main/java/com/project/reactor/time/TimeEat.java
@@ -12,31 +12,39 @@ public class TimeEat {
     public static void start() {
         //Date 与 LocalDateTime
 
-        //创建Date的可用方法 new Date(long milliSeconds);
+        //创建Date的可用方法 new Date(long milliSeconds); 根据时间戳 创建
         Date date = new Date(System.currentTimeMillis());
-        LocalDateTime localDateTime = LocalDateTime.of(2010, 10, 9, 10, 9, 9); //2010-10-09 10:09:09
-        long milliSeconds = localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli(); //+8时区 的毫秒值
+
+        long milliSeconds = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
         //OffsetDateTime odt = OffsetDateTime.now(ZoneId.systemDefault());
         //ZoneOffset defaultOffset = odt.getOffset();
-        //long milliSeconds = localDateTime.toInstant(defaultOffset).toEpochMilli(); //系统默认时区 的毫秒值
+        //long milliSeconds = localDateTime.toInstant(defaultOffset).toEpochMilli();
+        //创建Date
         new Date(milliSeconds);
-        //老代码中Date可以通过如上LocalDateTime方式构建
+
 
         //老代码中已有值Date转化为LocalDateTime
         Instant instant = date.toInstant();
         LocalDateTime.ofInstant(instant, ZoneOffset.of("+8"));
+        LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 
     }
 
     public static void localDateTime() {
+        //系统默认时区的当前时间, LocalDateTime本身并不存储时区信息，一旦构造，使用final int year等字段保存时间值
+        LocalDateTime.now();
 
-        LocalDateTime.now(); //使用系统默认时区的秒构建，此代码在本地使用本地系统的默认时区，在服务器使用服务器的默认时区，服务器有两台且两台的时区不同呢
-
-        //1 创建一个时间 2010-10-09 10:09:09 (LocalDateTime)
+        //创建LocalDateTime using 2010-10-09 10:09:09
         LocalDateTime localDateTime = LocalDateTime.of(2010, 10, 9, 10, 9, 9);
-        //2 转化为+8时区 的秒 (Instant)
-        Instant instant = localDateTime.toInstant(ZoneOffset.of("+8"));
-        instant.toEpochMilli(); //取毫秒值
+
+        //时间戳: 绝对时空的产物。如:
+        //自UTC时间 1970-01-01 00:00:00 -->  某一个UTC时间: 2021-07-10 04:16:24 经过的秒(/毫秒)
+        //而+8时间  1970-01-01 08:00:00 -->  +8时间: 2021-07-10 12:16:24      经过的秒(/毫秒)  时间戳与时区无关，是一个固定的值; 时间戳转化为时间需要指定在哪一个时区
+        //获取时间戳
+        System.currentTimeMillis(); //获取时间戳
+        localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli()); //获取时间戳: 确保系统默认时区是+8
+        System.out.println(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()); //默认时区的时间(LocalDateTime不存储时区信息)指定在默认时区，获取时间戳
 
 
         //通过秒 纳秒(相对值) 时区偏移 创建
