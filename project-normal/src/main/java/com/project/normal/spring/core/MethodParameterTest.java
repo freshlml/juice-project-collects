e5ea8025427d6a04b0f68647a9b9a3edc148bbde
fresh <1326191992@qq.com>
2021-09-06 00:16:20 +0800

    1

---
+++ project-normal/src/main/java/com/project/normal/spring/core/MethodParameterTest.java
@@ -0,0 +1,39 @@
+package com.project.normal.spring.core;
+
+import org.springframework.core.MethodParameter;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class MethodParameterTest {
+
+    public static void main(String argv[]) throws Exception {
+        MethodParameterTest testObj = new MethodParameterTest();
+        Method method = testObj.getClass().getMethod("test", new Class<?>[]{Map.class});
+
+        MethodParameter methodParameter = MethodParameter.forExecutable(method, 0);
+
+        Class<?> parameterType = methodParameter.getParameterType();
+        Type genericParameterType = methodParameter.getGenericParameterType();
+
+        methodParameter.increaseNestingLevel(); //or nested()
+        methodParameter.setTypeIndexForCurrentLevel(1);
+
+        Class<?> nestedParameterType = methodParameter.getNestedParameterType();
+        Type nestedGenericParameterType = methodParameter.getNestedGenericParameterType();
+
+
+        System.out.println("Hello");
+    }
+
+    public TempForMethodParameterTest test(Map<String, TempForMethodParameterTest> paramMap) {
+        return null;
+    }
+
+    private static class TempForMethodParameterTest {
+
+    }
+
+
+}
