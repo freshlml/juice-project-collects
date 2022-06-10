ca3e8ac991603662db313ab507958c98c84a8818
freshlml <1326191992@qq.com>
2022-06-10 17:54:43 +0800

    资源路径

---
+++ project-spring/src/main/java/com/juice/spring/core/io/ClasspathResLoaderTest.java
@@ -0,0 +1,94 @@
+package com.juice.spring.core.io;
+
+import java.net.URL;
+import java.util.Enumeration;
+
+public class ClasspathResourceTest {
+
+    public static void main(String argv[]) throws Exception {
+
+        loader();
+        test();
+
+    }
+
+    public static void loader() {
+        /**
+         * 使用Classloader加载资源
+         */
+        //bootstrap-classloader从sun.boot.class.path指定的路径中加载资源
+        //1. JDK安装目录\jre\lib\resources.jar、rt.jar、sunrsasign.jar、jsse.jar、jce.jar、charsets.jar、jfr.jar
+        //2. JDK安装目录\jre\classes
+        System.out.println("sun.boot.class.path: " + System.getProperty("sun.boot.class.path"));
+        //Ext-classloader从java.ext.dirs指定的路径中加载资源
+        //1. JDK安装目录\jre\lib\ext
+        //2. ~\Sun\Java\lib\ext，eg:windows上C:\WINDOWS\Sun\Java\lib\ext
+        System.out.println("java.ext.dirs: " + System.getProperty("java.ext.dirs"));
+        //1. App-Classloader从java.class.path指定的路径中加载资源
+        //  java.class.path的值:
+        //     如果通过C:\Users\DELL\Desktop>java -jar project-spring.jar
+        //              则java.class.path = user.dir = C:\Users\DELL\Desktop
+        //     如果是idea中运行,idea会把当前应用打包输出的路径写入java.class.path,如D:\ideaProject3\juice-project-collects\project-spring\target\classes
+        //              其行为和java -jar保持一致
+        //2. MANIFEST.MF的Class-Path指定路径
+        //idea中运行额外增加: JDK安装目录\jre\lib\charsets.jar、deploy.jar、javaws.jar、jce.jar、jfr.jar、jfxswt.jar、jsse.jar、management-agent.jar、plugin.jar、resources.jar、rt.jar
+        //idea中运行额外增加: JDK安装目录\jre\lib\ext\access-bridge-64.jar、cldrdata.jar、dnsns.jar、jaccess.jar、jfxrt.jar、localedata.jar、nashorn.jar、sunec.jar、sunjce_provider.jar、sunmscapi.jar、sunpkcs11.jar、zipfs.jar
+        //idea中运行，会将当前应用的pom.xml中依赖的jar包在本地仓库中的路径写入java.class.path
+        //java -jar运行，应用的pom.xml中依赖的jar包在本地仓库的路径不会写入了java.class.path (@link 项目打包方式)
+        System.out.println("java.class.path: " + System.getProperty("java.class.path"));
+        System.out.println("---------------------");
+
+        /**项目打包方式 */
+        //项目打成jar包，依赖jar包的内容合并到项目jar包
+
+
+        //项目打成jar
+        //依赖的jar包放到某个目录(可以是项目jar包内部目录或者磁盘的任意目录),需要在项目jar包中的MANIFEST.MF文件中通过Class-Path: ./lib/commons-loggings.jar 指定依赖的jar包位置
+        //App-Classloader能够从Class-Path加载资源
+
+
+        //maven项目打包(通过maven-jar-plugin插件打包)
+        //项目打成jar包，依赖的jar存在于本地仓库，项目jar包未关联上依赖的jar包
+        //  如果是在idea中运行，依赖jar包的本地仓库路径写入java.class.path
+        //  如果通过java -jar运行，依赖jar包的本地仓库路径不会写入java.class.path
+
+
+        //Spring Boot中打包(通过spring-boot-maven-plugin插件打包)
+        //项目打成jar包，项目clazz文件不位于jar包的顶层目录而是在BOOT-INF/classes/目录，MANIFEST.MF中指定Spring-Boot-Classes: BOOT-INF/classes/
+        //依赖的jar包放到项目jar包里面的BOOT-INF\lib目录下面，MANIFEST.MF中指定Spring-Boot-Lib: BOOT-INF/lib/
+        //自定义ClassLoader，继承App-Classloader
+
+        //因此如果一个非Spring Boot项目依赖Spring Boot项目
+        //无法加载Spring Boot项目中的类
+        //加载资源要加上BOOT-INF/classes/前缀,eg: BOOT-INF/classes/com/...
+    }
+
+
+    public static void test() throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        URL rtRe = classLoader.getResource("rt.jar");
+        //sun.boot.class.path写入的路径为: jdk安装目录\jre\lib\rt.jar，可以加载rt.jar包中的资源
+        //如果写入的路径是jdk安装目录\jre\lib\，则可加载rt.jar本身和rt.jar包中的资源
+        System.out.println(rtRe);  // null
+        System.out.println("-----------1----------");
+
+        //App-Classloader
+        URL res = classLoader.getResource("com/juice/spring/core/io/article.xml");
+        System.out.println(res); // file:/D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/core/io/article.xml
+        System.out.println("-----------2----------");
+
+        //不支持通配符
+        Enumeration<URL> ress = classLoader.getResources("com/juice/spring/core/io/**");
+        while(ress.hasMoreElements()) {
+            System.out.println(ress.nextElement());
+        }
+        System.out.println("-----------3----------");
+
+        ress = classLoader.getResources("com/fresh");
+        while(ress.hasMoreElements()) {
+            System.out.println(ress.nextElement());
+        }
+
+    }
+}
--- project-spring/src/main/java/com/juice/spring/core/io/PathsTest.java
+++ project-spring/src/main/java/com/juice/spring/core/io/PathsTest.java
@@ -1,23 +1,38 @@
 package com.juice.spring.core.io;
 
+import org.springframework.util.StringUtils;
+
 import java.io.File;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 
 public class PathsTest {
 
+    public static void main(String argv[]) throws Exception {
+        path_test();
+        System.out.println("-----------------------");
 
-    public static void main(String argv[]) {
+        url_path_test();
+        System.out.println("-----------------------");
 
+        test_clean_path();
+
+    }
+
+    public static void path_test() {
         System.out.println("user.dir: " + System.getProperty("user.dir"));
 
         /**
          * 相对路径
          * 相对于当前工作空间所在目录: System.getProperty("user.dir")
-         * C:\Users\DELL\Desktop>java -jar NormalProject.jar
-         * user.dir: C:\Users\DELL\Desktop
+         * user.dir的值:
+         *      C:\Users\DELL\Desktop>java -jar project-spring-1.0.0.jar
+         *      user.dir: C:\Users\DELL\Desktop
+         *
+         *      idea中运行: user.dir = D:\ideaProject3  (当前project目录非module目录)
          */
-        File f = new File("NormalProject.jar");
+        File f = new File("project-spring-1.0.0.jar");
         System.out.println(f.exists());
         System.out.println(f.getPath());
         Path path = f.toPath();
@@ -25,33 +40,118 @@ public class PathsTest {
         System.out.println(path.toString());
         System.out.println(path.isAbsolute());
         System.out.println(path.toAbsolutePath());
-        System.out.println(path.toUri());
+        System.out.println("-----------相对路径----------");
 
         /**
          * win机上绝对路径
-         * File f = new File("D:/ideaProject/NormalProject/pom.xml");
+         * File f = new File("D:/ideaProject3/juice-project-collects/pom.xml");
+         * File ff = new File("/D:/ideaProject3/juice-project-collects/pom.xml");
          */
-
         /**
          * /: linux机上绝对路径, 根目录
-         * /: win机上相对路径，相对于当前工作空间所在盘符
+         * /: win机上相对路径，相对于当前工作空间(user.dir)所在盘符
          */
-        File af = new File("/Users/DELL/Desktop/NormalProject.jar");
+        File af = new File("/Users/DELL/Desktop/project-spring-1.0.0.jar");
         System.out.println(af.exists());
-
+        System.out.println("-----------绝对路径----------");
+        //直接根据路径通常不太妙
 
         /**
          * 使用类加载器加载资源
-         * @see LoaderTest
+         * @see ClasspathResLoaderTest
          */
-        URL classesPath = PathsTest.class.getClassLoader().getResource("com/freshjuice/fl/resources/Resources.class");
-        System.out.println(classesPath);
-        URL classpathPath = PathsTest.class.getClassLoader().getResource("java/lang/String.class");
-        System.out.println(classpathPath);
-        URL jarRes = PathsTest.class.getClassLoader().getResource("javafx-mx.jar"); //貌似classpath上的文件不能被定位
-        System.out.println(jarRes);
+    }
+
+    public static void url_path_test() throws Exception {
+        /**
+         *URL路径:
+         *protocol://host:port/path?query
+         * 如: http://localhost:7001/user?id=1&name=q
+         *
+         *文件URL:
+         *  file:/path?query   host,port为空, path是文件的绝对路径
+         *windows
+         * file:/D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+         * jar:file:/C:/Program%20Files/Java/jdk1.8.0_202/jre/lib/rt.jar
+         * jar:file:/C:/Program%20Files/Java/jdk1.8.0_202/jre/lib/rt.jar!/java/lang/String.class
+         *
+         *linux
+         * file://ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+         *
+         */
+        /**
+         *URI路径
+         * 一般与URL路径没区别
+         */
+        URL url = new URL("http://localhost:7001/user?id=1&name=q");
+        System.out.println(url);   // http://localhost:7001/user?id=1&name=q
+        System.out.println(url.toURI());  // http://localhost:7001/user?id=1&name=q
+
+        url = new URL("http", "localhost", 7001, "user?id=1&name=q");
+        System.out.println(url);  // http://localhost:7001user?id=1&name=q
+        System.out.println(url.toURI()); // http://localhost:7001user?id=1&name=q
+
+        URI uri = URI.create("http://localhost:7001/user?id=1&name=q");
+        System.out.println(uri);         //  http://localhost:7001/user?id=1&name=q
+        System.out.println(uri.toURL()); //  http://localhost:7001/user?id=1&name=q
+        System.out.println(uri.getHost()); //   localhost
+        //note: getPath调用有前置/
+        System.out.println(uri.getPath()); //   /user
+        System.out.println(uri.getQuery()); //  id=1&name=q
+        System.out.println(uri.getSchemeSpecificPart());  // //localhost:7001/user?id=1&name=q
+        System.out.println("--------------1-----------");
+
+        //使用绝对路径构造，文件URL
+        url = new URL("file:/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java");
+        System.out.println(url);   //  file:/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+        System.out.println(url.toURI()); //  file:/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+        System.out.println(url.getHost()); // 空
+        //note: getPath调用有前置/
+        System.out.println(url.getPath()); //  /D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+        System.out.println("--------------2-----------");
+
+        //通过classloader加载资源，返回URL(文件URL)
+        URL uu = PathsTest.class.getClassLoader().getResource("com/juice/spring/App.class");
+        System.out.println(uu); //  file:/D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+        System.out.println(uu.toURI());  //  file:/D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+        System.out.println(uu.getProtocol()); // file
+        System.out.println(uu.getHost());     // 空
+        //note: getPath调用有前置/
+        System.out.println(uu.getPath()); //  /D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+        System.out.println(uu.toURI().getSchemeSpecificPart()); // /D:/ideaProject3/juice-project-collects/project-spring/target/classes/com/juice/spring/App.class
+        System.out.println("--------------3-----------");
+
+        //构造File,从File拿到绝对路径，构造URL
+        //构造File,Path.toURI调用
+        File f = new File("juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java");
+        System.out.println(f.getPath()); // juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+        Path path = f.toPath();
+        System.out.println(path.getFileName()); // App.java
+        System.out.println(path.isAbsolute());  // false
+        System.out.println(path.toAbsolutePath());  //D:\ideaProject3\juice-project-collects\project-spring\src\main\java\com\juice\spring\App.java
+
+        //note: 从Path得到的URI路径多了host前面的//
+        URI pu = path.toUri();
+        System.out.println(pu);  //  file:///D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+        System.out.println(pu.getHost());  // null
+        System.out.println(pu.getPath());  // /D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java
+
+    }
 
 
+    public static void test_clean_path() {
+
+        System.out.println(StringUtils.cleanPath("file:///D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+        System.out.println(StringUtils.cleanPath("jar:file:/C:/Program%20Files/Java/jdk1.8.0_202/jre/lib/rt.jar"));
+        System.out.println(StringUtils.cleanPath("jar:file:/C:/Program%20Files/Java/jdk1.8.0_202/jre/lib/rt.jar!/java/lang/String.class"));
+        System.out.println(StringUtils.cleanPath("file:/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+
+        System.out.println(StringUtils.cleanPath("/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+        System.out.println(StringUtils.cleanPath("D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+        System.out.println(StringUtils.cleanPath("juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+
+        System.out.println(StringUtils.cleanPath("./juice-project-collects/project-spring/src/main/java/com/juice/spring/App.java"));
+        System.out.println(StringUtils.cleanPath("../juice-project-collects/../project-spring/src/main/java/com/juice/spring/App.java"));
 
     }
 
