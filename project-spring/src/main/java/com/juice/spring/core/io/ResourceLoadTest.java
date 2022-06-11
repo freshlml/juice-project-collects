81d31644b987bebf75a9db56c8546a507d5498f8
freshlml <1326191992@qq.com>
2022-06-11 19:27:06 +0800

    资源加载测试

--- project-spring/src/main/java/com/juice/spring/core/io/ResourceLoadTest.java
+++ project-spring/src/main/java/com/juice/spring/core/io/ResourceLoadTest.java
@@ -3,8 +3,12 @@ package com.juice.spring.core.io;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import java.io.InputStream;
+import java.net.URL;
 
 public class ResourceLoadTest {
 
@@ -14,6 +18,9 @@ public class ResourceLoadTest {
         System.out.println("------------1----------");
 
         fileSystemResourceLoaderTest();
+        System.out.println("------------2----------");
+
+        patternLocationTest();
 
     }
 
@@ -21,15 +28,16 @@ public class ResourceLoadTest {
     public static void defaultResourceLoaderTest() throws Exception {
         DefaultResourceLoader defaultResourceLoader = new DefaultResourceLoader();
 
+        //1. /开头
         Resource oneRes = defaultResourceLoader.getResource("/com/juice/spring/core/io/article.xml");
         System.out.println(oneRes.getURL());
-
+        //2. classpath:
         Resource classPathRes = defaultResourceLoader.getResource("classpath:com/juice/spring/core/io/article.xml");
         System.out.println(classPathRes.getURL());
-
+        //3.URL格式资源路径
         Resource urlRes = defaultResourceLoader.getResource("file:/D:/ideaProject3/juice-project-collects/project-spring/src/main/java/com/juice/spring/core/io/article.xml");
         System.out.println(urlRes.getURL());
-
+        //3.URL格式资源路径
         Resource resource = defaultResourceLoader.getResource("jar:file:/E:/apache-maven-3.6.1/localRepository/com/freshjuice/fl/ArchIsomer/0.0.1-SNAPSHOT/ArchIsomer-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes/com/freshjuice/fl/base/mapper/Temp.Mapper.xml");
         System.out.println(resource.getURL());
 
@@ -37,9 +45,6 @@ public class ResourceLoadTest {
         byte[] bys = new byte[512];
         is.read(bys);
         System.out.println(new String(bys));
-
-        Resource defaultRes = defaultResourceLoader.getResource("com/juice/spring/core/io/article.xml");
-        System.out.println(defaultRes.getURL());
 
     }
 
@@ -56,5 +61,15 @@ public class ResourceLoadTest {
         System.out.println(new String(bys));
 
     }
+
+    public static void patternLocationTest() throws Exception {
+
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+
+        Resource[] result = resourcePatternResolver.getResources("jar:file:/D:/ideaProject3/juice-project-collects/project-spring/target/project-spring-1.0.0.jar!/**/*.xml");
+        System.out.println(result[0]);
+
+    }
+
 
 }
