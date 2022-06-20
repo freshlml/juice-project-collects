73f223652f211f54878b160970af9dd32843daa4
freshlml <1326191992@qq.com>
2022-06-21 01:03:09 +0800

    思考题7-2

--- project-alg/src/main/java/com/juice/alg/chapter2_7/Chapter2_7_2.java
+++ project-alg/src/main/java/com/juice/alg/chapter2_7/Chapter2_7_2.java
@@ -11,6 +11,25 @@ public class Chapter2_7_2 {
         //a = new int[]{1, 2, 3, 13};
         System.out.println(hoare_partition(a, 0, a.length));
         System.out.println(Arrays.toString(a));
+        System.out.println("-------------7-1-------------");
+
+        a = new int[]{13, 19, 9, 5, 12, 8, 7, 4, 12, 2, 6, 12};
+        Chapter2_7_2 chapter2_7_2 = new Chapter2_7_2();
+        try {
+            int[] qt = chapter2_7_2.range_partition(a, 0, a.length);
+            System.out.println("[q=" + qt[0] + ", t=" + qt[1] + ")");
+            System.out.println(Arrays.toString(a));
+        } catch (RangeOrderedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("------------7-2-partition------------");
+
+        a = new int[]{13, 19, 9, 5, 12, 8, 7, 4, 12, 2, 6, 12};
+        chapter2_7_2.range_quick_sort(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println("------------7-2-sort------------");
+
+
     }
 
     //思考题7-1
@@ -44,6 +63,70 @@ public class Chapter2_7_2 {
         return j;
     }
 
+
+    //思考题7-2
+    //a: n^2
+    //b: 序列中和基元相等的值无需参与后续递归
+    int[] range_partition(int a[], int begin, int end) {
+        int i = begin-1;
+        int l = i;
+        int j = begin;
+        int k = a[end-1];  //基元
+
+        int prev = begin-1;
+        boolean ordered = true;
+
+        while(j < end) {
+
+            if(a[j] == k) {
+                l++;
+                int ex = a[l];
+                a[l] = a[j];
+                a[j] = ex;
+            } else if(a[j] < k) {
+                int ex = a[j];
+                l++;
+                a[j] = a[l];
+                i++;
+                a[l] = a[i];
+                a[i] = ex;
+            } else {
+                ordered = false;
+            }
+
+            if(ordered && prev >= begin) {
+                if(a[prev] > a[j]) ordered = false;
+            }
+            prev = j;
+
+            j++;
+        }
+        if(ordered) throw new RangeOrderedException(begin, end, i+1);
+        return new int[]{i+1, l+1};
+    }
+    //c:
+    void range_quick_sort(int a[]) {
+        if(a == null) return;
+        if(a.length == 0 || a.length == 1) return;
+
+        range_quick_sort(a, 0, a.length);
+    }
+    void range_quick_sort(int a[], int begin, int end) {
+        if((end - begin) <= 1) return;
+
+        int[] q;
+        try {
+            q = range_partition(a, begin, end);
+        } catch (RangeOrderedException ro) {
+            //System.out.println(ro.getMessage());
+            return;
+        }
+        range_quick_sort(a, begin, q[0]);
+        range_quick_sort(a, q[1], end);
+    }
+    //d: range_partition返回的下标范围不参与后续的递归，序列中i与j(i<j)比较一次的概率
+    //   P(Xij=1) ~ [2/(j-i+1), 1]; 取P(Xij=1) = k*[2/(j-i+1)]， todo
+    //
 
 
 
