9c09e384cb8fa983493c224968a330af44ce8c80
freshlml <1326191992@qq.com>
2022-10-11 18:49:15 +0800

    存在相等元素时，select，random_select，求出来的顺序统计量值是对的，但在原序列中的下标是不定的

--- project-alg/src/main/java/com/juice/alg/chapter2_9/Chapter2_9_2.java
+++ project-alg/src/main/java/com/juice/alg/chapter2_9/Chapter2_9_2.java
@@ -18,9 +18,9 @@ public class Chapter2_9_2 {
      *
      *      check1: 互异
      *          if less == i-1: break //返回A[l]
-     *      check2: 非互异，相等有先后
+     *      check2: 非互异，相等的元素按出现的先后次序排位，依次确定顺序统计量
      *          if less + pre_eq == i-1: break //返回A[l],相等有先后的条件下下标l是准确的
-     *      check3: 非互异，相等无先后
+     *      check3: 非互异，相等元素可随意使用一组顺序统计量
      *          if less <= i-1 <= less + eq: break  //返回A[l], 真实的值在[l, l+eq]下标范围中
      *最坏情况运行时间：n^2
      *
@@ -60,25 +60,26 @@ public class Chapter2_9_2 {
      */
     /**
      *第三: partition
-     *  序列中存在相等元素时:
+     *序列中存在相等元素时:
      *   1.相等的元素按出现的先后次序排位，依次确定顺序统计量
-     *   2.或者，有些算法是不稳定的，将导致相等元素的排位次序改变，这样，一组相等元素可随意使用一组顺序统计量
+     *   2.或者，有些算法是不稳定的，将导致相等元素的排位次序改变，这样，求出来的顺序统计量值是对的，但在原序列中的下标是不定的
      *   3.或者认为，相等的元素是一个顺序统计量
-     *  partition版本 符合上述1
-     *  random_partition版本 符合上述2，可以考虑搭配range_partition返回一组下标
-     *  Chapter2_9_3的select 符合上述1，因为第57行代码从end开始
-     *  让算法符合3，可以考虑使用range_partition的思想来做适当修改
+     *  partition，random_partition 均不稳定，符合上述2，可以考虑搭配range_partition返回一组下标
      *
-     *  最坏情况运行时间: n^2
+     *  Chapter2_9_3的select 也不稳定，符合上述2
      *
-     *  平均情况运行时间分析:
+     *  要符合3，可以考虑使用range_partition的思想来做适当修改
+     *
+     *
+     *最坏情况运行时间: n^2
+     *
+     *平均情况运行时间分析:
      *     输入序列假设: n个数互异且均匀随机
      *
      *     X: partition次数，概率不好求
      *     X: 所有partition中总比较次数，partition执行多少次是随机的，不好求
      *
      *     todo
-     *
      *
      *  int random_partition(int a[], int begin, int end) {
      *     int r = RANDOM(begin, end) //begin,end下标之间等概率的产生一个下标值
