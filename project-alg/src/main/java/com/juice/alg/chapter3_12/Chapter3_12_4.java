4b6ec9dcbaa000b296ba5afc9dc82447468a83ff
freshlml <1326191992@qq.com>
2022-12-06 17:19:31 +0800

    思考题12-2

--- project-alg/src/main/java/com/juice/alg/chapter3_12/Chapter3_12_4.java
+++ project-alg/src/main/java/com/juice/alg/chapter3_12/Chapter3_12_4.java
@@ -26,6 +26,7 @@ public class Chapter3_12_4 {
      *   3.todo
      *    E[h] = O(lgn)
      *    练习，todo
+     *    思考题12-3，todo
      * 期望
      *   使用随机数生成器创造"均匀随机"序列，从而期望树高h = O(lgn)
      *   random_add(int[] es):
@@ -36,6 +37,45 @@ public class Chapter3_12_4 {
      *
      *
      */
+
+
+    //思考题12-2
+    /*
+    Node {
+        bit[] key;
+        Node left;
+        Node right;
+        Node parent;
+        boolean active;
+
+        Node(bit[] key, Node parent) {
+            this.key = key;
+            this.parent = parent;
+        }
+    }
+    build-tree(bit[][] S, Node root):
+        if(root == null) root = new Node(null, null);
+
+        for(int i=0; i<S.length; i++):
+            bit[] per = S[i];
+            Node next = root;
+            for(int j=0; j<per.length; j++):
+                if per[j] is bit'0:
+                    if(next.left == null):
+                        Node n = new Node(per[0...j], next);
+                        next.left = n
+                    next = next.left;
+                if per[j] is bit'1:
+                    if(next.right == null):
+                        Node n = new Node(per[0...j], next);
+                        next.right = n
+                    next = next.right;
+
+            next.active = true;
+    print(Node root):
+        按T_L_R打印active即可
+     */
+
 
 
 }
