ff18b69ba50c2dc09114dd7154e7b6d6c7dc0e1f
freshlml <1326191992@qq.com>
2023-11-07 22:08:49 +0800

    1

--- project-java/src/main/java/com/juice/jv/util/regexp/PatterningTest.java
+++ project-java/src/main/java/com/juice/jv/util/regexp/PatterningTest.java
@@ -7,8 +7,11 @@ public class PatterningTest {
 
     public static void main(String argv[]) {
 
+        matcher_test();
+        System.out.println("-------------------1----------------------------");
+
         classifier_test();
-        System.out.println("------------------------------------------------");
+        System.out.println("-------------------2-----------------------------");
 
         Pattern p = Pattern.compile("\\?|\\*|\\{(([^/{}])+?)\\}");
         Matcher m = p.matcher("{username:\\w}");
@@ -28,7 +31,44 @@ public class PatterningTest {
 
     }
 
-    public static void classifier_test() {
+    static void matcher_test() {
+        //
+        Pattern p = Pattern.compile("^.+?$", Pattern.DOTALL | Pattern.MULTILINE);
+        Matcher m = p.matcher("ab\r\nc");
+        boolean r = m.matches();
+        //true, matches 方法的语义是 match entire region, 与 regex 中 '^', '$' 和 Pattern.MULTILINE 矛盾
+        System.out.println(r);
+        /*
+        matches: ab
+        c
+         */
+        System.out.println("matches: " + m.group());
+
+        m.reset();
+        m.region(2, 5);
+        boolean rl = m.lookingAt();
+        System.out.println(rl);  //true
+        /* lookingAt 方法的语义是 starting at the beginning of the region, 与 regex 中 '^' 和 Pattern.MULTILINE 矛盾
+        lookingAt:
+        c
+         */
+        System.out.println("lookingAt: " + m.group());
+
+        m.reset();
+        while(m.find()) {
+            /*
+            region: start=0, end=5
+            find: ab
+            region: start=0, end=5
+            find: c
+             */
+            System.out.println("region: start=" + m.regionStart() + ", end=" + m.regionEnd());
+            System.out.println("find: " + m.group());
+        }
+
+    }
+
+    static void classifier_test() {
         //贪婪
         Pattern pattern = Pattern.compile("\\{([^/]+)\\}");
         String str = "{1abc}pol{2abc}\r\nlop{3abc}";
@@ -38,9 +78,9 @@ public class PatterningTest {
             System.out.println("matcher string: " + matcher.group());
             System.out.println("matcher position: " + "start=" + matcher.start() + ";end=" + matcher.end());
             System.out.println("matcher group1: " + matcher.group(1));
-            System.out.println("$$$$$$$$$$$$$$");
+            System.out.println("###############");
         }
-        System.out.println("--------------------------------------------------");
+        System.out.println("------------------------1.1--------------------------");
 
         //非贪婪
         String patternStr = "\\{([^/]+?)\\}";
