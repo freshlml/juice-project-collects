04b6f81db0aa1a485150097bb8f2988f1240f0ab
freshlml <1326191992@qq.com>
2024-11-14 12:48:08 +0800

    练习6.5-3、6.5-9

--- project-alg/src/main/java/com/juice/alg/part2/chapter6/Chapter6_5.java
+++ project-alg/src/main/java/com/juice/alg/part2/chapter6/Chapter6_5.java
@@ -2,18 +2,19 @@ package com.juice.alg.part2.chapter6;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Chapter6_5 {
 
     //(最大堆)最大优先队列
     static class MaxPriorityQueue {
-        private final List<Node> queue;
+        private final List<Node<Integer>> queue;
 
         public MaxPriorityQueue() {
             queue = new ArrayList<>();
         }
 
-        public void insert(Node node) {
+        public void insert(Node<Integer> node) {
             //assert node != null
             queue.add(node);
             heapify(this.queue.size()-1);
@@ -23,7 +24,7 @@ public class Chapter6_5 {
 
             int ci = i;
             int pi = (ci+1)/2 - 1;
-            Node i_node = this.queue.get(i);
+            Node<Integer> i_node = this.queue.get(i);
             while (pi >= 0 && this.queue.get(pi).lt(i_node)) {
                 this.queue.set(ci, this.queue.get(pi));
 
@@ -34,16 +35,16 @@ public class Chapter6_5 {
                 this.queue.set(ci, i_node);
         }
 
-        public Node max() {
+        public Node<Integer> max() {
             if (this.queue.size() == 0) return null;
             return this.queue.get(0);
         }
 
-        public Node next() {
+        public Node<Integer> next() {
             if (this.queue.size() == 0) return null;
             if(this.queue.size() == 1) return this.queue.remove(0);
 
-            Node ret = this.queue.get(0);
+            Node<Integer> ret = this.queue.get(0);
             this.queue.set(0, this.queue.remove(this.queue.size() - 1));
             max_heapify(0, this.queue.size());
             return ret;
@@ -63,27 +64,26 @@ public class Chapter6_5 {
             }
 
             if(max != i) {
-                Node ex = this.queue.get(max);
+                Node<Integer> ex = this.queue.get(max);
                 this.queue.set(max, this.queue.get(i));
                 this.queue.set(i, ex);
                 max_heapify(max, heap_size);
             }
         }
 
-        public void increase(Node node, int added) {
+        public void increase(Node<Integer> node, int added) {
+            for(int i=0; i<this.queue.size(); i++) {
+                Node<Integer> e;
+                if((e=this.queue.get(i)).equals(node)) {
 
-            for(int i=0 ;i<this.queue.size(); i++) {
+                    int old_weight = e.weight;
+                    int new_weight = old_weight + added;
+                    e.weight = new_weight;
 
-                if(this.queue.get(i).equals(node)) {
-
-                    int old_key = this.queue.get(i).key;
-                    int new_key = old_key + added;
-                    this.queue.get(i).key = new_key;
-
-                    if(old_key > new_key) {
+                    if(old_weight > new_weight) {
                         max_heapify(i, this.queue.size());
                     }
-                    if(old_key < new_key) {
+                    if(old_weight < new_weight) {
                         heapify(i);
                     }
 
@@ -96,52 +96,60 @@ public class Chapter6_5 {
         @Override
         public String toString() {
             return "MaxPriorityQueue{" +
-                    " queue=[" + queue.stream().map(node -> node.key+"").reduce((key1, key2) -> key1 + ", " + key2).orElse("") +
+                    " queue=[" + queue.stream().map(node -> node.weight +"").reduce((w1, w2) -> w1 + ", " + w2).orElse("") +
                     "] }";
         }
     }
 
-    static class Node {
-        int key;
-        int value;
-        Node(int key, int value) {
-            this.key = key;
+    static class Node<T> {
+        int weight;
+        final T value;
+        Node(int weight, T value) {
+            this.weight = weight;
             this.value = value;
         }
 
-        static Node build(int key, int value) {
-            return new Node(key, value);
+        static <T> Node<T> build(int weight, T value) {
+            return new Node<>(weight, value);
         }
 
-        public boolean gt(Node other) {
-            return other == null || this.key > other.key;
+        public boolean gt(Node<?> other) {
+            return other == null || this.weight > other.weight;
         }
 
-        public boolean lt(Node other) {
-            return other != null && this.key < other.key;
+        public boolean lt(Node<?> other) {
+            return other != null && this.weight < other.weight;
+        }
+
+        public int increaseWeight(int added) {
+            int oldWeight = this.weight;
+            this.weight += added;
+            return oldWeight;
         }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-            Node node = (Node) o;
-            boolean flag = key == node.key;
-            flag &= value == node.value;
-            return flag;
+            Node<?> node = (Node<?>) o;
+            /*boolean flag = weight == node.weight;
+            flag &= Objects.equals(value, node.value);
+            return flag;*/
+            return Objects.equals(value, node.value);
         }
 
         @Override
         public int hashCode() {
-            int hashCode = Integer.hashCode(key);
-            hashCode = 29*hashCode + Integer.hashCode(value);
-            return hashCode;
+            /*int hashCode = Integer.hashCode(oldWeight);
+            hashCode = 29*hashCode + Objects.hashCode(value);
+            return hashCode;*/
+            return Objects.hashCode(value);
         }
 
         @Override
         public String toString() {
             return "Node{" +
-                    "key=" + key +
+                    "weight=" + weight +
                     ", value=" + value +
                     '}';
         }
@@ -151,7 +159,7 @@ public class Chapter6_5 {
         MaxPriorityQueue maxPriorityQueue = new MaxPriorityQueue();
 
         maxPriorityQueue.insert(Node.build(16, 16));
-        Node node14 = Node.build(14, 14);
+        Node<Integer> node14 = Node.build(14, 14);
         maxPriorityQueue.insert(node14);
         maxPriorityQueue.insert(Node.build(10, 10));
         maxPriorityQueue.insert(Node.build(1, 1));
@@ -159,7 +167,7 @@ public class Chapter6_5 {
         maxPriorityQueue.insert(Node.build(8, 8));
         maxPriorityQueue.insert(Node.build(2, 2));
         maxPriorityQueue.insert(Node.build(7, 7));
-        Node node9 = Node.build(9, 9);
+        Node<Integer> node9 = Node.build(9, 9);
         maxPriorityQueue.insert(node9);
         maxPriorityQueue.insert(Node.build(3, 3));
 
@@ -174,11 +182,13 @@ public class Chapter6_5 {
 
         System.out.println(maxPriorityQueue);
         maxPriorityQueue.increase(node9, 6);
+        System.out.println("increase node9 6");
         System.out.println(maxPriorityQueue);
         System.out.println("#########################################################################");
 
         System.out.println(maxPriorityQueue);
         maxPriorityQueue.increase(node14, -12);
+        System.out.println("increase node14 -12");
         System.out.println(maxPriorityQueue);
 
     }
