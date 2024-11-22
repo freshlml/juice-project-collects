b0e23e6ed4088ea6a579b1dcdd4e1c02becd228d
freshlml <1326191992@qq.com>
2024-11-22 18:26:33 +0800

    加强

--- project-alg/src/main/java/com/juice/alg/part2/chapter8/Chapter8_4.java
+++ project-alg/src/main/java/com/juice/alg/part2/chapter8/Chapter8_4.java
@@ -1,101 +1,121 @@
 package com.juice.alg.part2.chapter8;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 
-import java.util.Arrays;
+import com.juice.alg.part1.chapter2.Chapter2.ArrayPrinter;
+import com.juice.alg.part2.chapter8.Chapter8_Practice.ArrayTraversal;
 
 public class Chapter8_4 {
 
     /**
      *桶排序思想
-     * 桶:  0   1   2   3   4   5 ... k-1   k
-     *     | | | | | | | | | | | |   |  |  | |
-     * n个数的序列 "映射" 桶下标
-     * 1.桶排序的关键是如何确定k的大小
-     * 2.确定映射关系，映射需要满足：大的数映射的桶下标不小于小的数映射的桶下标，hash散列就不适合
-     * 3.相同桶下标中数需要排序，如可以是链表结构在插入时排序
+     *  将 n 个数的序列映射到桶
+     *                             桶
+     *                          0 |   |
+     *                          1 |   |
+     *  A⟨ A1, A2, A3, ... An ⟩  2 |   |
+     *                            |...|
+     *                        k-1 |   |
+     *
+     *  1. 确定桶的大小 k
+     *     k <= n。完美情况是当 k == n 时，n 个数一人占一个位置。若 k < n，无疑增大了碰撞的概率
+     *  2. 确定映射关系。"定义域"为输入序列，"值域"为桶下标的集合
+     *     映射关系需满足: 大的数映射的桶下标不小于小的数映射的桶下标. (故，常见的 hash 散列此处不适合)
+     *  3. 桶下标碰撞处理: 对于映射到同一桶下标的数需要局部排序
+     *     如，可以用链表记录，每次发生碰撞时，执行插入排序
+     *  4. 将 n 个数映射到桶之后，遍历桶得到排序后的序列
      *
      *
-     *计数排序中应用桶思想
-     * 1.k的大小是确定的
-     * 2.桶下标映射 {@link Chapter8_2#indexPos(int, int)}，满足
-     * 3.√
+     *输入序列 A 满足 [0, 1) 上的均匀分布:
+     *  将 [0, 1) 分成 k 个子区间（称为 k 个桶），保证每个子区间长度相等，如:      [0, 1/k), [1/k, 2/k), ... ,[k-1/k, 1)
+     *                                                            桶下标：0，       1，              k-1     故而，桶下标映射关系为: Ai*k
      *
+     *  将输入序列 A 的 n 个元素映射到桶之中，因为 A 满足 [0, 1) 上的均匀分布，所以落在桶之中的数的数量一般比较均匀
      *
-     *A[n],A中任意一个数A[j]∈[0,1)，应用桶排序
-     * 1.将区间均等分成n份，得到n个区间: [0, 1/n), [1/n, 2/n), ... ,[n-1/n, 1)，k=n       ;@see 区间均等划分的过程
-     * 2.桶下标映射: A[j]*n
-     * 3.√
-     * 最快情况运行时间：所有数落在一个桶中，退化成插入排序
-     * 平均情况运行时间：
-     *      1.假设A[n]满足[0，1)上的均匀分布，则对A[n]中任意一个数A[j]落在i号桶的概率=1/n
-     *      2.随机变量ni: i号桶中元素数量
-     *        Σi(0,n-1)[ni^2]; 求期望E( Σi(0,n-1)[ni^2] ) = 2 - 1/n
-     *      3.平均情况运行时间O(n)
-     * 期望运行时间：真实情况是A[n]不一定满足[0，1)上的均匀分布
-     *      1.只要保证ni^2与n成线性关系，桶排序仍然线性？
+     *  注: 因为 A 满足的是 [0, 1) 上的均匀分布，所以才对 [0, 1) 区间的划分要保证每个子区间长度相等，这样可以保证桶中数的数量的均衡性
+     *     如果 A 满足的是其他分布，则对 [0, 1) 区间的划分方式不尽相同，但都有同一个目的: 使桶中数的数量更加均衡
      *
+     *  平均情况运行时间：
+     *    随机变量X: 内层循环的总循环次数  //等于 e.key <= nn.key 的执行次数
+     *    T(n) = Θ(n) + X
      *
-     *区间均等划分的过程，[0, 1)区间
-     * [0, t), [t, 2t), [2t, 3t), ... , [(m-1)t, mt)，共m个区间
-     * mt=1，t=1/m ==> [0, 1/m), [1/m, 2/m), [2/m, 3/m), ... , [(m-1)/m, m/m) ; 桶下标映射：t*m
+     *    X = Σ(i=0~n-2)Σ(j=i+1~n-1) X<i,j>,  其中 X<i,j> 表示 i 与 j 的比较次数 (i < j) = 0
+     *                                                                                = 1
+     *    P(X<i,j> = 1) = 1/n^2    //因为 A 满足 [0, 1) 上的均匀分布
      *
-     * 令m=n，共n个区间
-     *      [0, 1/n), [1/n, 2/n), [2/n, 3/n), ... , [(n-1)/n, n/n)
-     *      桶下标应映射 A[j] * n
+     *    EX = E[ Σ(i=0~n-2)Σ(j=i+1~n-1) X<i,j> ]
+     *       = Σ(i=0~n-2)Σ(j=i+1~n-1) EX<i,j>
+     *       = Σ(i=0~n-2)Σ(j=i+1~n-1) 1/n^2
+     *       = Θ(1)
+     *    故 T(n) = Θ(n) + Θ(1) = Θ(n)
      *
-     * 或者令m=根号n，共根号n个区间
-     *      [0, 1/根号n), [1/根号n, 2/根号n), [2/根号n, 3/根号n), ... , [(根号n-1)/根号n, 根号n/根号n)
-     *      桶下标应映射 A[j] * 根号n
+     *    另: 随机变量Yi: 第 i 个桶中元素个数. T(n) = Θ(n) + Σ(i=1~n) [Yi^2]
+     *
+     *等价描述: 输入序列 A 满足 [0, n) 上的均匀分布:
+     *  将 [0, n) 分成 k 个子区间（称为 k 个桶），保证每个子区间长度相等，如:    [0, n/k), [n/k, 2n/k), [2n/k, 3n/k), ... ,[((k-1)n)/k, n)
+     *                                                          桶下标：0，        1，          2，                k-1     故而，桶下标映射关系为: (Ai*k)/n
      */
-
-    public float[] bucket_sort(float[] a) {
+    public static double[] bucket_sort(double[] a, int k) {
         if(a == null) return null;
         if(a.length == 0 || a.length == 1) return a;
 
         int n = a.length;
-        Node[] bucket = new Node[n];
+        k = Math.min(k, n);
+        Node[] bucket = new Node[k];
         for(int j=0; j<n; j++) {
-            int idx = (int) a[j] * n;
+            int idx = (int) (a[j] * k);  //因为我们假定 a[j] ∈ [0, 1), 所以 a[j] * k ∈ [0, k)
             Node nn = new Node(a[j], null);
-            Node p;
-            if((p = bucket[idx]) == null) {
-                bucket[idx] = nn;
-            } else if(p.getKey() > nn.getKey()) {
-                bucket[idx] = nn;
-                nn.setNext(p);
+
+            Node prev = null;
+            Node e = bucket[idx];
+            while(e != null && e.key <= nn.key) {
+                prev = e;
+                e = e.next;
+            }
+            nn.next = e;
+            if(prev != null) {
+                prev.next = nn;
             } else {
-                while(p.getNext() != null && p.getNext().getKey() <= nn.getKey()) { //error: 浮点数的大小比较
-                    p = p.getNext();
-                }
-                Node ct = p.getNext();
-                p.setNext(nn);
-                nn.setNext(ct);
+                bucket[idx] = nn;
             }
         }
 
-        float[] b = new float[a.length];
-        for(int k=0, j=0; k<bucket.length; k++) {
+        double[] result = new double[n];
+        for(int i=0, j=0; i<k; i++) {
             Node p;
-            for(p = bucket[k]; p != null; ) {
-                b[j++] = p.getKey();
-                p = p.getNext();
+            for(p = bucket[i]; p != null; ) {
+                result[j++] = p.key;
+                p = p.next;
             }
         }
 
-        return b;
+        return result;
+    }
+
+    public static void main(String[] argv) {
+        double[] d = new double[] {0.1, 0.01, 0.101, 0.211001, 0.9990001, 0.99999999, 0.987654321};
+        ArrayTraversal.of(d).forEach(ArrayPrinter.of()::print);
+        double[] result = bucket_sort(d, 10);
+        ArrayTraversal.of(result).forEach(ArrayPrinter.of()::print);
+
+        System.out.println("---------------------------------------");
+
+        int[] a = new int[] {6, 5, 5, 2, 1, 3, 6, 7};
+        int[] b = counting_sort_using_bucket(a, 0, 10);
+        ArrayTraversal.of(a).forEach(ArrayPrinter.of()::print);
+        ArrayTraversal.of(b).forEach(ArrayPrinter.of()::print);
 
     }
 
-    //练习8.4-3: E[X*Y] = E[X]*E[Y];X,Y独立
+    //练习8.4-2: 探查 k 次，找空位
+    //  (n/k)^2 = n*lgn, k = (n/lgn)^(1/2)
+
+    //练习8.4-3: E[X*Y] = E[X] * E[Y] 当 X, Y 相互独立时
     //          E^2[X] = E[X^2] - 方差
 
     //练习8.4-4
     /*
+    Π(r2)^2 - Π(r1)^2 = Π(r1)^2, 即 r2 = 根号2 * r1, 以此类推:
+
     区间划分                                     根号m * r1 = 1 ==> r1 = 1/根号m             下标映射
     [0, r1)                      1号区间         [0, 1/根号m)                               (d * 根号m)^2 --> 0
     [r1, 根号2 * r1)              2号区间         [1/根号m, 根号2/根号m)                       (d * 根号m)^2 --> 1
@@ -104,85 +124,78 @@ public class Chapter8_4 {
     ...
     [根号m-1 * r1, 根号m * r1)     m号区间         [根号m-1/根号m, 根号m/根号m)                  (d * 根号m)^2 --> m-1
 
-    可以令 m=n, 共n个区间
+    可令 m = n, 共 n 个区间
         [0, 1/根号n), [1/根号n, 根号2/根号n), [根号2/根号n, 根号3/根号n), ... , [根号n-1/根号n, 根号n/根号n) ; 下标映射 (d * 根号n)^2
 
-    或者令 m=n^2, 共n^2个区间
+    或者令 m = n^2, 共 n^2 个区间
         [0, 1/n), [1/n, 根号2/n), [根号2/n, 根号3/n), ... , [根号n-1/n, 根号n/n) ; 下标映射 (d * n)^2
 
      */
 
     //练习8.4-5 todo
 
-    public int[] bucket_sort_for_counting(int[] a, int begin, int end) {
+
+    public static int[] counting_sort_using_bucket(int[] a, int begin, int end) {
         if(a == null) return null;
         if(a.length == 0 || a.length == 1) return a;
 
-        //assert begin < end
-
-        Counting_Node[] bucket = new Counting_Node[end - begin];
+        CountingNode[] bucket = new CountingNode[end - begin];
 
         for(int i=0; i<a.length; i++) {
-            int idx = counting_index(begin, a[i]);
+            int idx = counting_index(a[i], begin);
 
-            Counting_Node p;
-            Counting_Node nn = new Counting_Node();
-            nn.setKey(a[i]).setTail(nn);
-            if((p = bucket[idx]) == null) {
+            CountingNode head;
+            CountingNode nn = new CountingNode(a[i], null, null);
+            if((head = bucket[idx]) == null) {
                 bucket[idx] = nn;
+                nn.next = nn.prev = nn;
             } else {
-                p.getTail().setNext(nn);
-                p.setTail(nn);
+                CountingNode tail = head.prev;
+                tail.next = nn;
+                nn.prev = tail;
+
+                head.prev = nn;
+                nn.next = head;
             }
         }
 
         int[] b = new int[a.length];
         for(int k=0, j=0; k<bucket.length; k++) {
-            Counting_Node p;
-            for(p = bucket[k]; p != null; ) {
-                b[j++] = p.getKey();
-                p = p.getNext();
+            CountingNode p, head = bucket[k];
+            for(p = head; p != null; ) {
+                b[j++] = p.key;
+                p = p.next;
+                if(p == head) break;
             }
         }
 
         return b;
     }
-    public int counting_index(int begin, int ai) {
-        if(begin == 0) return ai;
-        else if(begin > 0) return ai - begin;
-        else return ai + begin;
+    static int counting_index(int ai, int min) {
+        return ai - min;
     }
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Accessors(chain = true)
-    private static class Counting_Node {
-        private int key;
-        private Counting_Node next;
-        private Counting_Node tail;
+    private static class CountingNode {
+        int key;
+        CountingNode next;
+        CountingNode prev;
+
+        CountingNode(int key, CountingNode next, CountingNode prev) {
+            this.key = key;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Accessors(chain = true)
     private static class Node {
-        private float key;
-        private Node next;
+        double key;
+        Node next;
+
+        Node(double key, Node next) {
+            this.key = key;
+            this.next = next;
+        }
     }
 
-
-    public static void main(String argv[]) {
-        Chapter8_4 chapter2_8_4 = new Chapter8_4();
-
-        int[] a = new int[]{6, 5, 5, 2, 1, 3, 6, 7};
-        int[] b = chapter2_8_4.bucket_sort_for_counting(a, 0, 10);
-        Arrays.stream(b).forEach(System.out::println);
-
-
-    }
 
 }
