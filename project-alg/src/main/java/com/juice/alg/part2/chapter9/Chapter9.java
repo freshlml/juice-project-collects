1b6d65962c86ddd750e3207e3cc0cc7b88f8a24a
freshlml <1326191992@qq.com>
2024-11-27 12:38:21 +0800

    1

--- project-alg/src/main/java/com/juice/alg/part2/chapter9/Chapter9.java
+++ project-alg/src/main/java/com/juice/alg/part2/chapter9/Chapter9.java
@@ -13,9 +13,9 @@ public class Chapter9 {
      *  若序列中包含重复元素，则某个顺序统计量可能不是唯一的，即重复元素均可作为该顺序统计量
      *
      *中位数
-     *  当 n 为奇数时，中位数是唯一的: i = (n+1)/2, 其中 i 表示顺序统计量
-     *  当 n 为偶数时，下中位数为: i = n/2 = ⌊(n+1)/2⌋, 其中 i 表示顺序统计量
-     *              上中位数为: i = n/2 + 1 = ⌊(n+2)/2⌋, 其中 i 表示顺序统计量
+     *  当 n 为奇数时，中位数是唯一的: 第 (n+1)/2 个顺序统计量
+     *  当 n 为偶数时，下中位数为: 第 n/2 = ⌊(n+1)/2⌋ 个顺序统计量
+     *              上中位数为: 第 n/2 + 1 = ⌊(n+2)/2⌋ 个顺序统计量
      */
     public static void main(String[] argv) {
         chapter9_1();
@@ -24,153 +24,175 @@ public class Chapter9 {
      *chapter 9.1
      *
      *找最值
-     * min(int[] A) {
-     *     min = A[0]
-     *     for i from 1 to n-1:
-     *         if min > A[i]:
-     *             min = A[i]
-     * } //比较n-1次
-     * minimum(int[] A, int begin, int end) {
-     *     n = end - begin
-     *     if n == 1: return A[begin]
+     *  ①
+     *   min(int[] A) {
+     *       min = A[0]
+     *       for i from 1 to n-1:
+     *           if min > A[i]:
+     *               min = A[i]
+     *   }
+     *   T(n) = Θ(1) * (n-1) = Θ(n)
      *
-     *     left = minimum(A, begin, begin + n/2)
-     *     right = minimum(A, begin + n/2, end)
+     *  ②
+     *   minimum(int[] A, int begin, int end) {
+     *       n = end - begin
+     *       if n == 1: return A[begin]
      *
-     *     if left <= right: return left
-     *     else: return right
-     * } //比较n-1次
-     *找最值比较n-1次是渐进最优的: n个数候选，每比较一次排除一个
+     *       left = minimum(A, begin, begin + n/2)
+     *       right = minimum(A, begin + n/2, end)
+     *
+     *       if left <= right: return left
+     *       else: return right
+     *   }
+     *   画出递归树: T(n) = 1*Θ(1) + 2*Θ(1) + 4*Θ(1) + ... + 2^(x-2) * Θ(1) + 2^(x-1) * Θ(1)  + n*Θ(1), x = lgn
+     *                  = Θ(1) * (2n - 1)
+     *                  = Θ(n)
+     *
      *
      *同时找最大值和最小值
-     * min = A[0]
-     * max = A[0]
-     * for i from 1 to n-1:
-     *     if min > A[i]:
-     *         min = A[i]
-     *     if max < A[i]:
-     *        max = A[i]
-     *总比较次数: 2 * (n-1)
+     *  ①
+     *    min = A[0]
+     *    max = A[0]
+     *    for i from 1 to n-1:
+     *        if min > A[i]:
+     *            min = A[i]
+     *        if max < A[i]:
+     *           max = A[i]
      *
-     * min = A[0]  //n=A.length为奇数
-     * max = A[0]
-     * for i from 1 to n-2 step by 2:
-     *     if A[i] <= A[i+1]:
-     *         if min > A[i]:
-     *             min = A[i]
-     *         if max < A[i+1]:
-     *             max = A[i+1]
-     *     else:
-     *         if min > A[i+1]:
-     *             min = A[i+1]
-     *         if max < A[i]:
-     *             max = A[i]
-     *总计较次数: 3*n/2
+     *    总比较次数: 2 * (n-1)
      *
-     *同时找最大值和最小值的比较次数的下界(练习9.1-2)
-     * 初始: 最大值候选n，最小值候选n
-     * 第一次比较: A1 < A2，比较一次，可以确定当前最大值最小值
-     * 第二次比较: A3 < A4，比较 1 + 2(3个位置选两个) 次，可以确定当前最大值最小值
-     * ...
-     * 第i次比较: Ai < Ai+1，比较 1 + 2(3个位置选两个) 次，可以确定当前最大值最小值
-     *总比较次数: 1 + 3 + 3 + ... + 3
-     *  n为奇数: 0 + (n-1)/2 * 3 = 3n/2 - 3/2
-     *  n为偶数: 1 + (n-2)/2 * 3 = 3n/2 - 2
+     *  ②
+     *   A.length为奇数:
+     *      min = A[0]
+     *      max = A[0]
+     *      start = 1
+     *   A.length为偶数:
+     *      if A[0] > A[1]:
+     *          min = A[1]
+     *          max = A[0]
+     *      else:
+     *          min = A[0]
+     *          max = A[1]
+     *      start = 2
+     *   end = n - 2
      *
+     *   for i from start to end step by 2:
+     *       if A[i] <= A[i+1]:
+     *           if min > A[i]:
+     *               min = A[i]
+     *           if max < A[i+1]:
+     *               max = A[i+1]
+     *       else:
+     *           if min > A[i+1]:
+     *               min = A[i+1]
+     *           if max < A[i]:
+     *               max = A[i]
+     *
+     *   总计较次数:                                                               //练习9.1-2
+     *     A.length为奇数: 1 + (⌊n/2⌋ - 1) * 3 = ⌊n/2⌋ * 3 - 2
+     *     A.length为偶数: ⌊n/2⌋ * 3
      */
     static void chapter9_1() {}
 
 
-    /*练习9.1-1
-     *1: 除了min之外的最小为第二小
-     * min = A[0]
-     * for i from 1 to n-1:
-     *     if min > A[i]:
-     *         min = A[i]
-     * min2 = MAX
-     * for i from 0 to n-1:
-     *     if A[i] == min: continue
-     *     if min2 > A[i]:
-     *         min2 = A[i]
-     * assert min2 != MAX
-     *比较次数: n-1 + n
+    //练习9.1-1
+    /*1:
+     *  min = A[0]
+     *  im = 0
+     *  for i from 1 to n-1:
+     *      if min > A[i]:
+     *          min = A[i]
+     *          im = i
+     *  SWAP A[0], A[im]
      *
-     *2: 根据min的信息，同时找min2
-     * min = A[0]
-     * min2 = MAX
-     * for i from 1 to n-1:
-     *     if min > A[i]:
-     *         min2 = min
-     *         min = A[i]
-     *     else if min < A[i]:
-     *         if min2 > A[i]:
-     *             min2 = A[i]
-     * assert min2 != MAX
-     *最坏情况序列: min,...
-     *最坏情况比较次数: 2*(n-1)
+     *  min2 = A[1]
+     *  for i from 2 to n-1:
+     *      //if A[i] == min: continue
+     *      if min2 > A[i]:
+     *          min2 = A[i]
      *
-     *3: 2 * (n-1)
-     * minimum_1_2(int[] A, int begin, int end) {
-     *     n = end - begin
-     *     if n == 1: return int[2] {A[begin], MAX}
+     *比较次数: n-1 + n-2
      *
-     *     left = minimum_1_2(A, begin, begin + n/2)
-     *     right = minimum_1_2(A, begin + n/2, end)
      *
-     *     r = int[2]
-     *     for(i=0, j=0, l=0; l<2; l++) {
-     *         if(left[i] < right[j]) {
-     *             r[l] = left[i]
-     *             i++
-     *         } else if(left[i] > right[j]) {
-     *             r[l] = right[j]
-     *             j++
-     *         } else {
-     *             r[l] = left[i]
-     *             i++
-     *             j++
-     *         }
-     *     }
+     *2:
+     *  if A[0] <= A[1]:
+     *      min  = A[0]
+     *      min2 = A[1]
+     *  else:
+     *      min  = A[1]
+     *      min2 = A[0]
      *
-     *     return r
-     * }
+     *  for i from 2 to n-1:
+     *      if min2 > A[i]:
+     *          if min <= A[i]:
+     *              min2 = A[2]
+     *          else:
+     *              min2 = min
+     *              min = A[2]
      *
-     *4: n + lgn - 2
-     *1.创建比较树
-     * create_tree(int[] A, int begin, int end) {
-     *     n = end - begin
-     *     if n == 1: return Node{A[begin], null, null}
+     *比较次数: 1 + 2 * (n-2)
      *
-     *     left = minimum(A, begin, begin + n/2)
-     *     right = minimum(A, begin + n/2, end)
      *
-     *     node = Node()
-     *     node.value = Min(left.value, right.value)
-     *     node->l = left
-     *     node->r = right
+     *3:
+     *  minimum_1_2(int[] A, int begin, int end) {
+     *      n = end - begin
+     *      if n == 1: return int[2] {A[begin], MAX}
      *
-     *     return node
-     * }
-     * eg: 5 3 9 7 6 2 1 3
-     *                 1
-     *        3                  1
-     *   3        7         2         1
-     * 5   3    9   7     6   2     1   3
+     *      left = minimum_1_2(A, begin, begin + n/2)
+     *      right = minimum_1_2(A, begin + n/2, end)
      *
-     *2.遍历tree
-     *  p = head
-     *  candidate = MAX
-     *  while(p.l != null && p.r != null) {
-     *      current_candidate = Max(p.l.value, p.r.value)
-     *      if current_candidate < candidate: candidate = current_candidate
-     *      p = p.l.value < p.r.value ? p.l : p.r
+     *      r = int[2]
+     *      for(i=0, j=0, l=0; l<2; l++) {
+     *          if(left[i] <= right[j]) {
+     *              r[l] = left[i]
+     *              i++
+     *          } else {
+     *              r[l] = right[j]
+     *              j++
+     *          }
+     *      }
+     *      return r
      *  }
+     *画出递归树，比较次数 = 1*2 + 2*2 + 4*2 + ... + 2^(x-1) * 2, x = lgn
+     *                 = 2 * (n-1)
+     *         T(n) = 1*2 + 2*2 + 4*2 + ... + 2^(x-1) * 2 + Θ(n) = 2 * (n-1) + Θ(n)
      *
      *
+     *4: 比较次数 = n + ⌈lgn⌉ - 2‌
+     *Ⅰ.创建比较树
+     *  class Node {int value, Node l, Node r}
      *
+     *  create_tree(int[] A, int begin, int end) {
+     *      n = end - begin
+     *      if n == 1: return Node{A[begin], null, null}
+     *
+     *      left = minimum(A, begin, begin + n/2)
+     *      right = minimum(A, begin + n/2, end)
+     *
+     *      node = Node()
+     *      node.value = Min(left.value, right.value)
+     *      node->l = left
+     *      node->r = right
+     *
+     *      return node
+     *  }
+     *  eg: 5 3 9 7 6 2 1 3
+     *                  1
+     *         3                  1
+     *    3        7         2         1
+     *  5   3    9   7     6   2     1   3
+     *
+     *Ⅱ.遍历tree
+     *  p = create_tree(A, 0, A.length)
+     *  if p.l ==null && p.r == null: return p
+     *
+     *  candidate = null
+     *  while(p.l != null && p.r != null) {
+     *      current_candidate = p.l.value >= p.r.value ? p.l : p.r
+     *      if candidate == null or current_candidate.value < candidate.value: candidate = current_candidate
+     *      p = p.l.value >= p.r.value ? p.r : p.l
+     *  }
+     *  return candidate
      */
-
-
-
 
 }
