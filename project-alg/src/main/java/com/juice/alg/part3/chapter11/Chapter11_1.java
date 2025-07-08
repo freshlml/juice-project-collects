a691ee0b4ce6764c307a7e451f8a81277c9ec537
freshlml <1326191992@qq.com>
2025-07-08 12:11:38 +0800

    1

--- project-alg/src/main/java/com/juice/alg/part3/chapter11/Chapter11_1.java
+++ project-alg/src/main/java/com/juice/alg/part3/chapter11/Chapter11_1.java
@@ -7,13 +7,14 @@ public class Chapter11_1 {
 
     /**
      *直接寻址表
-     *  关键字的全域 U 为 [m, n)。其中 m < n ∈ Z。创建一个长度为 n-m 的数组, 该数组的下标和全域 U 中每一个关键字一一对应。
+     *  关键字的全域 U 为 [m, n)。其中 m < n ∈ Z。创建一个长度为 n - m 的数组, 该数组的下标和全域 U 中每一个关键字一一对应。
      *  现有一个序列, 其元素的关键字取自全域 U。序列中元素即可直接寻址（一个简单的下标运算函数）找到对应的数组下标。
      *
      *  若全域 U 很大, 则该数组需要占用更多的空间。
      *  如果一个序列中元素只取全域 U 中很小一部分的值, 则数组中实际存储的关键字数目比关键字总数小得多, 数组的多数空间是浪费的。
      *
      */
+    @SuppressWarnings("unused")
     private static class DirectAddressTable<V> {
         private final Node<V>[] table;
         private final int from, to;
@@ -23,6 +24,7 @@ public class Chapter11_1 {
          * 构造一个直接寻址表
          * @param from 全域 U 的起始数值, inclusive
          * @param to 全域 U 的终止数值, exclusive
+         * @throws IllegalArgumentException if the specified `from` is greater than or equal to the specified `to`
          */
         @SuppressWarnings("unchecked")
         DirectAddressTable(int from, int to) {
@@ -31,6 +33,15 @@ public class Chapter11_1 {
             this.from = from;
             this.to = to;
             this.table = new Node[to - from];
+        }
+
+        /**
+         * Returns the number of key-value mappings in this table.
+         *
+         * @return the number of key-value mappings in this table
+         */
+        public int size() {
+            return size;
         }
 
         /**
@@ -43,7 +54,7 @@ public class Chapter11_1 {
             insertVal(key, v, false);
         }
 
-        private void insertVal(int key, V v, boolean writeIfAbsent) {
+        private void insertVal(int key, V v, boolean onlyIfAbsent) {
             Node<V> added = new Node<>(key, v);
             int idx = mapping(key);
             Node<V> e = table[idx], pe = null;
@@ -57,7 +68,7 @@ public class Chapter11_1 {
                 } else {
                     pe.next = added;
                 }
-            } else if(!writeIfAbsent) {
+            } else if(!onlyIfAbsent) {
                 e.v = v;
                 return;
             }
@@ -120,11 +131,11 @@ public class Chapter11_1 {
         }
 
         static class Node<V> {
-            int key;   //关键字∈全域 U. 也可以是其他类型, 但需要额外的一层转换, 如 Object.hashCode().
+            int key;   // 关键字 key ∈ 全域 U. key 可以是非整数类型, 则需要额外的一层运算将对象转换成整数, 如 Object#hashCode().
             V v;
             Node<V> next;
 
-            public Node(int key, V v) {
+            Node(int key, V v) {
                 this.key = key;
                 this.v = v;
             }
@@ -140,7 +151,7 @@ public class Chapter11_1 {
             return max.key;
         }
 
-        private Node<V> findMax(int p, int q) {
+        /*private Node<V> findMax(int p, int q) {
             int n = q - p;
             if(n <= 0) return null;
 
@@ -155,13 +166,24 @@ public class Chapter11_1 {
             } else {
                 return findMax(p, mid);
             }
+        }*/
+        private Node<V> findMax(int p, int q) {
+            int n = q - p;
+            if(n <= 0) return null;
+            if(n == 1) return table[p];
+
+            int mid = p + n/2;
+            Node<V> right = findMax(mid, q);
+            if(right != null) return right;
+            return findMax(p, mid);
         }
+
     }
 
     //练习11.1-2
     /*
     下标  0  1  2  3  4  5  6  7  8  9  10  11
-    存值  0  1  1  0  1  1  1  0  0  1   0   0
+    存值  0  1  1  0  1  1  1  0  0  1  0   1
          ↑  ↑  ↑     ↑     ↑        ↑
     元素  0  1  2     3     4        5
 
@@ -170,15 +192,7 @@ public class Chapter11_1 {
         则 5 的起始下标为 1 + 1 + 2 + 2 + 3 = 9
      */
 
-    /**
-     *关于 "散列表"、"哈希映射表"、"映射表" 这几种说法
-     *  "散列表" 一般就是指 "哈希映射表", 是采用 hash 散列函数完成映射的 "映射表"
-     *
-     *映射表: 将序列中元素通过 "映射函数" 映射到 "桶" 之中。一个映射表有以下几个方面需要关注:
-     *  1. 映射函数
-     *  2. 桶
-     *  3. 碰撞: 多个元素映射到同一个桶
-     */
-    @SuppressWarnings("unused")
-    static void mapping() {}
+
+    //一般地，"散列表" 也被叫做 "哈希映射表"、"映射表"；"散列函数" 也被叫做 "映射函数"
+    
 }
