c8f71dfdead25bdc2a7893333fda26a77349857f
freshlml <1326191992@qq.com>
2026-02-04 20:24:49 +0800

    1

--- juice-java/src/main/java/com/fresh/juice/jv/JvApplication.java
+++ juice-java/src/main/java/com/fresh/juice/jv/JvApplication.java
@@ -2,7 +2,7 @@ package com.fresh.juice.jv;
 
 public class JvApplication {
 
-    public static void main(String argv[]) {
+    public static void main(String[] argv) {
         System.out.println("hello 中");
     }
 }
