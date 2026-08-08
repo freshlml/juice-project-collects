5a84701c244ef18c29c7b8032077fc9c2ccea0a6
freshlml <1326191992@qq.com>
2026-08-08 11:24:12 +0800

    完善

--- juice-java/src/main/java/com/fresh/juice/jv/lang/numerical/Exercise4.java
+++ juice-java/src/main/java/com/fresh/juice/jv/lang/numerical/Exercise4.java
@@ -3,11 +3,19 @@ package com.fresh.juice.jv.lang.numerical;
 public class Exercise4 {
 
     public static void main(String[] argv) {
-        examine(16_777_215.9999f);   //16777216
-        examine(16_777_215.0001f);   //16777215
-        //1111 0000 0000 0000 0000 0000 |0011.000000...
-        examine(251_658_243.0001f);   //整数部分精度丢失: 251658240 == 1111 0000 0000 0000 0000 0000 |0000
+        examine(Float.NaN);                        //NaN
+        examine(Float.POSITIVE_INFINITY);          //POSITIVE INFINITY
+        examine(Float.NEGATIVE_INFINITY);          //NEGATIVE INFINITY
 
+        //0.1111_1110_0111_1011_1110_111
+        float f = -0x0.fe_7b_eeP-126f;
+        examine(f);                                //-0.11111110011110111110111 * 2^-126
+
+        //1.1111_0001_0001_0001_1111_111
+        float f2 = 0x1.f1_11_feP5f;               //1.11110001000100011111111 * 2^5
+        examine(f2);
+
+        System.out.println("--------------------------------------");
         System.out.println(calPolynome(10));
         System.out.println(calPolynome(100));
     }
@@ -29,17 +37,24 @@ public class Exercise4 {
                 System.out.println("NEGATIVE INFINITY");  // result = -(1 << 24)?
             }
         } else if(E == 0) {  //非规格化数
-            System.out.println(0);
+            //print S 0.M * 2^-126
+            StringBuilder sb = new StringBuilder();
+            if(S != 0) sb.append("-");
+            sb.append("0.");
+            sb.append(Integer.toBinaryString(M));
+            sb.append(" * 2^-126");
+            System.out.println(sb);
         } else {  //规格化数
             //计算指数值
             int raw_e = (E - 0x3f_80_00_00) & 0x7f_80_00_00;
             int e = raw_e << 1 >> 24;
-
-            if(e > 24) {
-                System.out.println("整数部分精度丢失: " + (int) f);
-            } else {
-                System.out.println((int) f);
-            }
+            //print S 1.M * 2 ^ e
+            StringBuilder sb = new StringBuilder();
+            if(S != 0) sb.append("-");
+            sb.append("1.");
+            sb.append(Integer.toBinaryString(M));
+            sb.append(" * 2^").append(e);
+            System.out.println(sb);
         }
     }
 
